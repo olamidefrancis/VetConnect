e072025d518db08cd63259b5f0355c5617046f0a
deji2612 <olatigbay@gmail.com>
2025-09-03 19:05:17 +0100

    Navbar design

---
+++ src/Componnets/Navbar/Navbar.jsx
@@ -0,0 +1,67 @@
+import './Navbar.css'
+import { Link } from 'react-router'
+import paw from '../../assets/Images/icons8-paw-100.png'
+
+
+
+
+function Navbar() {
+    const burgermi=()=>{
+        
+        let nav=document.querySelector('.main__navbar--content--linkcon');
+        let burgerFirstLine=document.querySelector('.line1');
+        let burgerSecondLine=document.querySelector('.line2');
+
+        
+            burgerFirstLine.classList.toggle('line1-toggled');
+            burgerSecondLine.classList.toggle('line2-toggled');
+
+            nav.classList.toggle('restore');
+        
+    }
+
+       
+    return (
+        <div className="main__navbar">
+            <div className="main__navbar--content">
+
+                <figure className='flexy'>
+                    <img src={paw} alt="logo" />
+                </figure>
+
+                <div className="main__navbar--content--linkcon flexy">
+                    <Link to='/Home' className='main__navbar--content--linkcon--home'>
+                        Home
+                    </Link>
+
+                    <Link to='/About' className='main__navbar--content--linkcon--about'>
+                        About
+                    </Link>
+
+                    <Link to='/Contact' className='main__navbar--content--linkcon--about'>
+                        Contact
+                    </Link>
+
+                    <Link to='/Login'className='main__navbar--content--logandreg--Login  log'>
+                        Log in 
+                    </Link>
+                </div>
+
+                <div className="humburgers" onClick={burgermi}>
+                    <div className="line1">
+
+                    </div>
+                    <div className="line2">
+
+                    </div>
+                </div>
+
+               
+
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
+
